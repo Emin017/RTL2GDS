2df6dea7d75206cb0e57835ca0c647c883c3b595
Qiming Chu <cchuqiming@gmail.com>
2025-06-14 17:32:13 +0800

    feat: add density evaluation to floorplan tcl script
    * Add run_density_eval command to floorplan Tcl script
    * Include density evaluation with configurable grid size
    * Output results to DESIGN_EVAL_REPORT directory
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- tools/iEDA/script/iFP_script/run_iFP.tcl
+++ tools/iEDA/script/iFP_script/run_iFP.tcl
@@ -118,6 +118,11 @@ source $IEDA_TCL_SCRIPT_DIR/DB_script/db_init_sdc.tcl
 run_timing_eval -eval_output_path $::env(DESIGN_EVAL_REPORT) -routing_type $::env(ROUTING_TYPE)
 
 #===========================================================
+##   run density evaluation
+#===========================================================
+run_density_eval -eval_output_path $::env(DESIGN_EVAL_REPORT) -grid_size $::env(DENSITY_GRID_SIZE) -stage "floorplan"
+
+#===========================================================
 ##   Exit 
 #===========================================================
 flow_exit
